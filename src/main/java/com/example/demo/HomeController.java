5b7302915b7d449073a5c8de1505b708aec5fe26
greendb16 <greendb16@gmail.com>
2019-07-11 15:37:45 -0400

    HTML Posts

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,8 +1,7 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
